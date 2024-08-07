905388261fd1f5312bbe177b66f791a6156d3cd0
jxaaa <gixian000@gmail.com>
2024-08-07 17:01:10 +0700

    add function admin1LevelData (Global IDP Data at Admin 1 Level).

--- R/admin1LevelData.R
+++ R/admin1LevelData.R
@@ -1,28 +1,17 @@
-#' Global IDP Data Country Level
+#' Global IDP Data at Admin 1 Level
 #'
-#' @param operation String, specifying the operation to perform.
-#' @param countryName String, specifying the name of country.
-#' @param admin0Pcode String, specifying the ISO Country Code.
-#' @param fromDate String, specifying the start date of the reporting period (format: "yyyy-mm-dd"), optional.
-#' @param toDate String, specifying the start date of the reporting period (format: "yyyy-mm-dd"), optional.
-#' @param monthFrom_month String, specifying the start month of the reporting period. Between 1 and 12.
-#' @param monthFrom_year Integer, specifying the start year of the reporting period. Year must be 4 digit.
-#' @param monthTo_month Integer, specifying the end month of the reporting period. Between 1 and 12.
-#' @param monthTo_year Integer, specifying the end year of the reporting period. Year must be 4 digit.
-#' @param roundFrom Integer, specifying the start round number, optional. Not less than 0.
-#' @param roundTo Integer, specifying the end round number, optional. Not less than 0.
-#' @param to_dataframe Boolean, Convert the response to a DataFrame, default to False, optional.
-#'
+#' @inheritParams countryLevelData
 #' @return A json file or dataframe.
 #'
 #' @import dplyr httr jsonlite
 #' @export
 #'
 #' @examples
-#' countryLevelData(admin0Pcode="MNG", monthFrom_month= "1", monthFrom_year=2000, monthTo_month= "12", monthTo_year=2023,to_dataframe =TRUE)
-countryLevelData <- function(operation = "", countryName = "", admin0Pcode = "", fromDate = NULL, toDate = NULL,
-                             monthFrom_month = NULL, monthFrom_year = NULL, monthTo_month = NULL,
-                             monthTo_year = NULL, roundFrom = NULL, roundTo = NULL, to_dataframe = FALSE) {
+#' admin1LevelData(admin0Pcode="MNG", monthFrom_month= "1", monthFrom_year=2000, monthTo_month= "12", monthTo_year=2023, to_dataframe = TRUE)
+
+admin1LevelData <- function(operation = "", countryName = "", admin0Pcode = "", fromDate = NULL, toDate = NULL,
+                            monthFrom_month = NULL, monthFrom_year = NULL, monthTo_month = NULL,
+                            monthTo_year = NULL, roundFrom = NULL, roundTo = NULL, to_dataframe = FALSE) {
   # Check if all required parameters are provided
   if (countryName == "" && admin0Pcode == "") {
     stop("Please provide either the countryName or the admin0Pcode")
@@ -43,7 +32,7 @@ countryLevelData <- function(operation = "", countryName = "", admin0Pcode = "",
     stop("Please provide the end year of the reporting period (int)")
   }
   # The URL for getting admin0 data
-  url <- "https://dtmapi.iom.int/api/IdpAdmin0Data/GetAdmin0Data"
+  url <- "https://dtmapi.iom.int/api/IdpAdmin1Data/GetAdmin1Data"
   # The request body
   payload <- list(
     operation = operation,
